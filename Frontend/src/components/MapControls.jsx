f283f9d8045d832a11fc66aa2610bfe9935f79fa
fedy <fedy.cherif26@cuny.edu>
2024-10-05 16:04:24 -0400

    Fixed a few bugs in MapControls

--- Frontend/src/components/MapControls.jsx
+++ Frontend/src/components/MapControls.jsx
@@ -10,9 +10,9 @@ const MapControls = (props) => {
     <div className="block">
       <div className="grid grid-cols-1 gap-x-6 sm:grid-cols-6">
         <div className="sm:col-span-3">
-          <label for="starting-point" className="block text-sm font-medium leading-6 text-gray-900">Origin</label>
+          <label htmlFor="starting-point" className="block text-sm font-medium leading-6 text-gray-900">Origin</label>
           <div className="">
-            <input type="text" name="starting-point" id="starting-point" autocomplete="given-name" className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={originPoint} onChange={(e) => {
+            <input type="text" name="starting-point" id="starting-point" autoComplete="given-name" className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={originPoint} onChange={(e) => {
           
               
               setOriginPoint(e.target.value);
@@ -22,9 +22,9 @@ const MapControls = (props) => {
         </div>
       
         <div className="sm:col-span-3">
-          <label for="ending-point" className="block text-sm font-medium leading-6 text-gray-900">Destination</label>
+          <label htmlFor="ending-point" className="block text-sm font-medium leading-6 text-gray-900">Destination</label>
           <div className="">
-            <input type="text" name="ending-point" id="ending-point" autocomplete="family-name" className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={destinationPoint} onChange={(e) => {
+            <input type="text" name="ending-point" id="ending-point" autoComplete="family-name" className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={destinationPoint} onChange={(e) => {
           
               
               setDestinationPoint(e.target.value);
