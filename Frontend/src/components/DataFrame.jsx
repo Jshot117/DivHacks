b1b21da83d9dd37cd4e4b013f5276ff0566fdfce
Jshot117 <jonnjstart@gmail.com>
2024-10-06 10:49:28 -0400

    fixed send mail functionality

--- Frontend/src/components/DataFrame.jsx
+++ Frontend/src/components/DataFrame.jsx
@@ -57,10 +57,7 @@ Sincerely,
         }}></textarea>
         
         <a 
-          href={"mailto:" + email + new URLSearchParams({
-            body: emailText,
-            subject: "Petition for Expanding Bike Lanes in Our District"
-          }).toString()}
+          href={`mailto:${email}?subject=Petition%20for%20Expanding%20Bike%20Lanes%20in%20Our%20District&body=${encodeURIComponent(emailText)}`}
           className = "bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center mt-4 dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400 "
         >Send Email</a>
 
