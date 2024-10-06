509ce78272b7b428841c7030986c0630fe0970d6
fedy <fedy.cherif26@cuny.edu>
2024-10-05 21:18:08 -0400

    Confirmed Get Data on Map Controls

--- Frontend/src/components/MapControls.jsx
+++ Frontend/src/components/MapControls.jsx
@@ -64,6 +64,8 @@ const MapControls = (props) => {
       </div>
       <button className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center mt-4 dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400" onClick={function(){
         
+        getData();
+        
       }}>
         Get Distance
       </button>
